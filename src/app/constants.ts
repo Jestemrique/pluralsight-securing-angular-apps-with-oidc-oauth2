d4aba32d6ebf69f66a8a1811f919592bcd67b65a
jestemrique <enrique.pastrana@gmail.com>
2022-12-18 11:30:07 +0100

    Initial comit

---
+++ src/app/constants.ts
@@ -0,0 +1,15 @@
+export class Constants {
+	public static clientRoot = 'http://localhost:4200/';
+
+	public static apiRoot = 'https://securingangularappscoursev2-api-unsecure.azurewebsites.net/api/';
+	//public static stsAuthority = 'https://securingangularappscoursev2-sts.azurewebsites.net/';
+
+	public static stsAuthority = 'https://epr-react-auth01-dev.eu.auth0.com/';
+
+
+	//public static apiRoot = 'https://localhost:2112/api/';
+	//public static stsAuthority = 'https://localhost:4242/';
+
+	//public static clientId = 'spa-client';
+	public static clientId = 'eiKPa9Lm2fHTlTmZ8EBNAQhqbOu19l7R';
+}
